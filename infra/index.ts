937e18c44996182203844d4b7a7463d6f4d7e987
synoet <teonys@nyu.edu>
2022-12-17 12:25:20 -0500

    ADD Trying to get ingress running

--- infra/index.ts
+++ infra/index.ts
@@ -18,7 +18,7 @@ const apiDeployment = new k8s.apps.v1.Deployment("api-deployment", {
         containers: [
           {
             name: "api",
-            image: "mc-api:latest",
+            image: "localhost:5000/mc-api:latest",
             ports: [
               {
                 containerPort: 8000,
@@ -33,15 +33,73 @@ const apiDeployment = new k8s.apps.v1.Deployment("api-deployment", {
 
 const apiService = new k8s.core.v1.Service("api-service", {
   metadata: {
+    name: "api-service",
     labels: {
       app: "api",
     },
   },
   spec: {
-    type: "LoadBalancer",
+    type: "NodePort",
     ports: [{ port: 8000, targetPort: 8000 }],
     selector: {
       app: "api",
     },
+  },
+});
+
+const ingressConfig = new k8s.core.v1.ConfigMap("ingress-config", {
+  metadata: {
+    name: "ingress-config",
+  },
+  data: {
+    "routes.yaml": `
+      - match:
+					path:
+						exact: "/ide"
+          headers:
+            x-cluster-ip:
+              regex: ".*"
+        route:
+          cluster: "{{ normalize(header(X-Cluster-IP)) }}"
+    `,
+  },
+});
+
+const ingress = new k8s.networking.v1.Ingress("mc-router", {
+  metadata: {
+    name: "mc-router",
+    annotations: {
+      "pulumi.com/skipAwait": "true",
+      "kubernetes.io/ingress.class": "traefik",
+      "traefik.ingress.kubernetes.io/router.entrypoints": "http",
+			"kubernetes.io/ingress.class": "nginx",
+      // "traefik.ingress.kubernetes.io/router.tls.certresolver": "default",
+      // "traefik.ingress.kubernetes.io/router.tls.tls": "true",
+      // "traefik.ingress.kubernetes.io/router.configmap": "ingress-config",
+    },
+  },
+  spec: {
+    rules: [
+      {
+        host: "minicube-local.dev",
+        http: {
+          paths: [
+            {
+              path: "/",
+              pathType: "Prefix",
+              backend: {
+                service: {
+                  name: "api-service",
+                  port: {
+                    number: 80,
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+		ingressClassName: ''
   },
 });
